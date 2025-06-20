ccafebf1cd79e831fe4483f51994b431e50e83c3
Vineeth V <teamminers999@gmail.com>
2025-06-20 21:15:38 +0530

    Working Gen AI Code

---
+++ questions2.js
@@ -0,0 +1,61 @@
+
+
+    const questions = [
+        {
+            "id": 1,
+            "question": "What is the name of the corporation that hires V and Jackie to steal the biochip?",
+            "options": [
+                "Nomad Corp",
+                "StreetCorp",
+                "Arasaka Corporation",
+                "CorpoTech"
+            ],
+            "correct_option": "Arasaka Corporation"
+        },
+        {
+            "id": 2,
+            "question": "Who is the iconic rock star and terrorist whose digital ghost haunts V after they awaken in the landfill?",
+            "options": [
+                "Johnny Silverhand",
+                "Eddie Winter",
+                "Kerry Eurodyne",
+                "Johnny Danger"
+            ],
+            "correct_option": "Johnny Silverhand"
+        },
+        {
+            "id": 3,
+            "question": "Which character, a member of the Aldecaldos nomad clan, helps V track down Arasaka's head doctor?",
+            "options": [
+                "Panam Palmer",
+                "Nomad King",
+                "Takemura Goro",
+                "Fixer McMahon"
+            ],
+            "correct_option": "Panam Palmer"
+        },
+        {
+            "id": 4,
+            "question": "What is the name of the AI created by Johnny's girlfriend Alt Cunningham, capable of copying netrunners' minds through their neural links?",
+            "options": [
+                "Soulkiller",
+                "Mindkeeper",
+                "Ghost Protocol",
+                "Neuroclone"
+            ],
+            "correct_option": "Soulkiller"
+        },
+        {
+            "id": 5,
+            "question": "What choice does V have to make regarding the Relic, leading to different endings and outcomes in the game?",
+            "options": [
+                "Surrender to Arasaka",
+                "Become a Night City legend",
+                "Leave Night City with Aldecaldos",
+                "All of the above"
+            ],
+            "correct_option": "All of the above"
+        }
+    ];
+
+    [END]
